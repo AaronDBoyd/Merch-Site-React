7379b8461419a44247b17752534c42c4d09c8d69
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 12:40:44 -0700

    setup NewMerchForm

---
+++ src/components/NewMerchForm.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReusableForm from './ReusableForm'
+import { v4 } from 'uuid'
+
+function NewMerchForm(props) {
+
+  function handleNewMerchFormSubmission(event) {
+    event.preventDefault();
+    props.onNewMerchCreation({
+      name: event.target.name.value,
+      description: event.target.description.value,
+      quantity: event.target.email.value,
+      id: v4()
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <h1>- Add Merch -</h1>
+      <ReusableForm
+        formSubmissionHandler={handleNewMerchFormSubmission}
+        buttonText='Add Merch'
+      />
+    </React.Fragment>
+  )
+}
+
+NewMerchForm.propTypes = {
+  onNewMerchCreation: PropTypes.func
+}
+
+export default NewMerchForm
