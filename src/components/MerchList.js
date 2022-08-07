3342382ab3b8ca76d9960808c48d81a1322b1859
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 12:46:16 -0700

    update MerchList

--- src/components/MerchList.js
+++ src/components/MerchList.js
@@ -2,36 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import Merch from "./Merch";
 
-const tempMerchList = [
-  {
-    name: "t-shirt",
-    description: "A shirt with a logo of Zara.",
-    quantity: 3,
-  },
-  {
-    name: "sticker",
-    description: "A sticker with a graphic of Zara floating through space.",
-    quantity: 6,
-  },
-  {
-    name: "kazoo",
-    description: "A kazoo that sounds just like Zara's singing voice.",
-    quantity: 27,
-  },
-];
+// const tempMerchList = [
+//   {
+//     name: "t-shirt",
+//     description: "A shirt with a logo of Zara.",
+//     quantity: 3,
+//   },
+//   {
+//     name: "sticker",
+//     description: "A sticker with a graphic of Zara floating through space.",
+//     quantity: 6,
+//   },
+//   {
+//     name: "kazoo",
+//     description: "A kazoo that sounds just like Zara's singing voice.",
+//     quantity: 27,
+//   },
+// ];
 
 function MerchList(props) {
   return (
     <React.Fragment>
       <h2>MerchList</h2>
       <hr />
-      {tempMerchList.map((merch, index) => (
+      {props.merchList.map((merch) => (
         <Merch
           whenMerchClicked={props.onMerchSelection}
           name={merch.name}
           description={merch.description}
           quantity={merch.quantity}
-          key={index}
+          id={merch.id}
+          key={merch.id}
         />
       ))}
     </React.Fragment>
@@ -39,6 +40,7 @@ function MerchList(props) {
 }
 
 MerchList.propTypes = {
+  merchList: PropTypes.array,
   onMerchSelection: PropTypes.func
 };
 
