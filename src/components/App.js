6a3099fab61f87f32d19c7cf1629245aff120424
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 11:08:58 -0700

    setup basic Header and Control components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+import MerchControl from './MerchControl'
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <MerchControl />
+    </React.Fragment>
+  )
+}
