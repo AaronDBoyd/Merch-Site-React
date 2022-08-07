c1fc543c5b24d79f4f4fc1d684ab9e310ba7eb31
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 12:12:00 -0700

    pass down whenMerchClicked function

--- src/components/MerchList.js
+++ src/components/MerchList.js
@@ -1,38 +1,45 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Merch from './Merch'
+import React from "react";
+import PropTypes from "prop-types";
+import Merch from "./Merch";
 
 const tempMerchList = [
   {
     name: "t-shirt",
     description: "A shirt with a logo of Zara.",
-    quantity: 3
+    quantity: 3,
   },
   {
-    name: "sticker", 
+    name: "sticker",
     description: "A sticker with a graphic of Zara floating through space.",
-    quantity: 6
+    quantity: 6,
   },
   {
-    name: "kazoo", 
+    name: "kazoo",
     description: "A kazoo that sounds just like Zara's singing voice.",
-    quantity: 27
-  }
-]
+    quantity: 27,
+  },
+];
 
 function MerchList(props) {
   return (
     <React.Fragment>
       <h2>MerchList</h2>
       <hr />
-      {tempMerchList.map((merch, index) =>
-      <Merch name = {merch.name} description = {merch.description} key = {index} />
-      )}
+      {tempMerchList.map((merch, index) => (
+        <Merch
+          whenMerchClicked={props.onMerchSelection}
+          name={merch.name}
+          description={merch.description}
+          quantity={merch.quantity}
+          key={index}
+        />
+      ))}
     </React.Fragment>
-    
-  )
+  );
 }
 
-MerchList.propTypes = {}
+MerchList.propTypes = {
+  onMerchSelection: PropTypes.func
+};
 
-export default MerchList
+export default MerchList;
