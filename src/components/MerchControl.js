81cd17ced1d3965fbf5f62cfb0e1bf5cdacd199c
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 11:48:10 -0700

    return MerchList

--- src/components/MerchControl.js
+++ src/components/MerchControl.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import MerchList from './MerchList'
 
 export default class MerchControl extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false,
+    }
+  }
+
   render() {
     return (
-      <div>MerchControl</div>
+      <React.Fragment>
+        <MerchList />
+      </React.Fragment>
     )
   }
 }
