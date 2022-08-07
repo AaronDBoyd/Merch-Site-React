50b6351fd8897c8d53bdbd72190c6a6785005cd8
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 12:16:05 -0700

    apply whenMerchClicked prop

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -1,21 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
 function Merch(props) {
   return (
     <React.Fragment>
-      <h3>{props.name}</h3>
-      <h4>{props.description}</h4>
-      <h4>Items Left: {props.quantity}</h4>     
+      <div onClick = {() => props.whenMerchClicked(props.key)}> {/* will change to props.id */}
+        <h3>{props.name}</h3>
+        <h4>{props.description}</h4>
+        <h4>Items Left: {props.quantity}</h4>
+      </div>
       <hr />
     </React.Fragment>
-  )
+  );
 }
 
 Merch.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string,
-  quantity: PropTypes.number
-}
+  quantity: PropTypes.number,
+};
 
-export default Merch
+export default Merch;
