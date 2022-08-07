321fcb5e9882cc1d745551a9b2c30d267e9650d0
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-07 12:55:16 -0700

    setup edit merch form

---
+++ src/components/EditMerchForm.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReusableForm from './ReusableForm'
+
+
+function EditMerchForm(props) {
+  const { merch } = props;
+
+  function handleEditMerchFormSubmission(event) {
+    event.preventDefault();
+    props.onEditMerch({
+      name: event.target.name.value,
+      description: event.target.description.value,
+      quantity: event.target.quantity.value,
+      id: merch.id
+    })
+  }
+
+  return (
+    <React.Fragment>
+      <h1>- Edit Merch -</h1>
+      <ReusableForm 
+        formSubmissionHandler={handleEditMerchFormSubmission}
+        buttonText="Update Merch"
+      />
+    </React.Fragment>
+  )
+}
+
+EditMerchForm.propTypes = {
+  merch: PropTypes.object,
+  onEditMerch: PropTypes.func
+
+}
+
+export default EditMerchForm
