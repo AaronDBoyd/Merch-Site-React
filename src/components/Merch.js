e4f76f42d7bcff7a262fae3a3df00ff7bc961ee9
Aaron Boyd <a.boyd91@yahoo.com>
2022-08-08 19:56:48 -0700

    inactivate button and change text if merch quantity is 0

--- src/components/Merch.js
+++ src/components/Merch.js
@@ -2,6 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Merch(props) {
+  let buyButtonVisible = null;
+  let buttonText = null;
+
+  if (props.quantity > 0) {
+    buyButtonVisible = (() => props.whenBuyClicked(props.id));
+    buttonText = "Buy Item";
+  } else {
+    buttonText = "Out of Stock";
+  }
+
   return (
     <React.Fragment>
       <div onClick = {() => props.whenMerchClicked(props.id)}> 
@@ -9,8 +19,7 @@ function Merch(props) {
         <h4>{props.description}</h4>
         <h4>Items Left: {props.quantity}</h4>
       </div>
-      <button onClick={() => props.whenBuyClicked(props.id)}>Buy Item</button>
-      {/* <button onClick={() => props.whenBuyClicked()}>Buy Item</button> */}
+      <button onClick={buyButtonVisible}>{buttonText}</button>
       <hr />
     </React.Fragment>
   );
